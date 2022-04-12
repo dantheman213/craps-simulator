cd24284ad2435501852b3aecfe26c8c97cd75280
Dan Gillespie <daniel.gillespie0949@gmail.com>
2022-04-12 00:12:59 -0700

    adding betting strategies. fixed random nums

--- main.go
+++ main.go
@@ -10,22 +10,45 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	sim := NewCrapsSim(200, 100)
+	sim.SetBettingStrategy(BettingStrategyPassLine, 15)
+	sim.SetBettingStrategy(BettingStrategyOdds, 15)
+	sim.SetBettingStrategy(BettingStrategyPoint6, 6)
+	sim.SetBettingStrategy(BettingStrategyPoint8, 6)
 	sim.Start()
 }
 
 func generateRandomNumber(min, max int) int {
-	return rand.Intn(max-min) + min
+	return rand.Intn(max-min+1) + min
 }
 
 type Craps struct {
-	isPuckOn     bool
-	Bankroll     float64
-	iteration    int
-	MaxIteration int
-	round        int
-	Die1         int
-	Die2         int
-	PointValue   int
+	isPuckOn          bool
+	Bankroll          float64
+	iteration         int
+	MaxIteration      int
+	round             int
+	Die1              int
+	Die2              int
+	PointValue        int
+	BettingStrategies *[]BettingStrategy
+}
+
+const (
+	BettingStrategyPoint4       = 4
+	BettingStrategyPoint5       = 5
+	BettingStrategyPoint6       = 6
+	BettingStrategyPoint8       = 8
+	BettingStrategyPoint9       = 9
+	BettingStrategyPoint10      = 10
+	BettingStrategyFieldBet     = 20
+	BettingStrategyPassLine     = 21
+	BettingStrategyDontPassLine = 22
+	BettingStrategyOdds         = 23
+)
+
+type BettingStrategy struct {
+	StrategyType int
+	Bet          float64
 }
 
 func NewCrapsSim(startBalance float64, maxIterations int) *Craps {
@@ -46,6 +69,18 @@ func (c *Craps) rollDice() {
 
 func (c *Craps) getDiceValue() int {
 	return c.Die1 + c.Die2
+}
+
+func (c *Craps) SetBettingStrategy(StrategyType int, bet float64) {
+	strat := BettingStrategy{
+		StrategyType: StrategyType,
+		Bet:          bet,
+	}
+
+	if c.BettingStrategies == nil {
+		c.BettingStrategies = &[]BettingStrategy{}
+	}
+	*c.BettingStrategies = append(*c.BettingStrategies, strat)
 }
 
 func (c *Craps) Start() {
@@ -82,6 +117,7 @@ func (c *Craps) Start() {
 
 				fmt.Printf("[Round %d][Iteration %d] Rolled point value (win!). End of round.\n", c.round, c.iteration)
 				c.isPuckOn = false
+				c.PointValue = 0
 				c.round += 1
 			} else {
 				// TBA
